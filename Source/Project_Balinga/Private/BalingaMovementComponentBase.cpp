574ce4a2b12bc9cf606cd2bc495fc0ee429b326e
Prince Kodjo <137556793+Prankalanka@users.noreply.github.com>
2025-05-22 21:43:13 +0100

    20% done with basics of fly movement mode
    Nothing functional added but:
    - Added more members to BalingaMovementComponent
    - Started the physFly function and partially added equations using those new members to calculate each force
    
    -Unsure of many things, most of which is described in the comments, but especially movement safety
    - I think everything that might change throughout runtime needs to be movement safe
    - Gonna figure out all the questions like I did the previous ones
    - Sad that I can't easily test some stuff since there's quite a few other movement mode functions that need to exist for a custom movement mode to even work

--- Source/Project_Balinga/Private/BalingaMovementComponentBase.cpp
+++ Source/Project_Balinga/Private/BalingaMovementComponentBase.cpp
@@ -11,9 +11,48 @@ bool UBalingaMovementComponentBase::IsCustomMovementMode(ECustomMovementMode InC
 	return MovementMode == MOVE_Custom && CustomMovementMode == InCustomMovementMode;
 }
 
+void UBalingaMovementComponentBase::PhysFly(float deltaTIme, int32 Iterations)
+{
+	if (deltaTime < MIN_TICK_TIME)
+	{
+		return;
+	}
+
+	// Add root motion code here if we ever decide to use root motions
+
+	// Thrust is applied in the direction the character is facing
+	// Unsure how deceleration works
+	if (safe_bFlapInput)
+	{
+		// actorForward only changes in the Z axis by default, we need to allow it to be changed in all during the fly mode
+		safe_actorForward = CharacterOwner->GetActorForwardVector();
+		Velocity += ((safe_flapInitThrust / Mass) * safe_actorForward) * safe_thrustScale * deltaTIme;
+
+		safe_bFlapInput = false;
+	}
+
+
+	safe_horizontalVelocity = FMath::Sqrt(FMath::Square(Velocity.X) + FMath::Square(Velocity.Y));
+
+	// baseForce and airFlowVelocity might need to become vectors, depending on how we calculate drag 
+	float baseForce = ((safe_airDensity * (safe_horizontalVelocity + safe_airflowVelocity)) / 2.0f) * safe_surfaceArea;
+
+	// Need to somehow factor in angleOfAttack (actorForwardVector, might get rid of either one)
+	// The closer to the airFlowDirection the greater maybe. But then upwards aiming wouldn't lift you as much
+	float lift = baseForce * safe_liftScale;
+
+	float drag = baseForce * safe_dragScale;
+
+	Velocity += (lift / Mass) * FVector::UpVector * deltaTIme; // Lift is always applied upwards (change if we add changing gravity direction)
+
+	// What direction should drag apply in? Both horizontal axes? Only one? The one airFlowVelocity is referring to?
+	Velocity += (drag / Mass) * safe_actorForward * deltaTIme; 
+}
+
 void UBalingaMovementComponentBase::InitializeComponent()
 {
 	Super::InitializeComponent();
 
 	BalingaOwner = Cast<ABalingaBase>(GetOwner());
 }
+
--- Source/Project_Balinga/Public/BalingaMovementComponentBase.h
+++ Source/Project_Balinga/Public/BalingaMovementComponentBase.h
@@ -5,11 +5,15 @@
 #include "BalingaBase.h"
 #include "BalingaMovementComponentBase.generated.h"
 
+// Similar to EMovemementMode in EngineTypes.h, none and max are there out of convention
 UENUM(BlueprintType)
 enum ECustomMovementMode
 {
-	CMOVE_None UMETA(Hidden),
+	CMOVE_None UMETA(Hidden), 
+
+	// Fly and glide might become one depending on if they handle physics the same way
 	CMOVE_Fly UMETA(DisplayName = "Fly"),
+	CMOVE_Glide UMETA(DisplayName = "Glide"),
 	CMOVE_MAX UMETA(Hidden),
 };
 
@@ -24,15 +28,52 @@ public:
 private:
 	GENERATED_BODY() 
 
+	// Holds data used in phys functions that might change and thus need to be remembered to accurately replicate moves on the server
 	class FSavedMove_Balinga : FSavedMove_Character
 	{
+	public: 
+		float saved_thrustScale;
+		float saved_liftScale;
+		float saved_dragScale;
+
+		int8 saved_angleOfAttack;
+		int8 saved_surfaceArea;
+		int8 saved_airDensity;
+		
+		bool saved_bFlapInput;
+
+		FVector saved_actorForwardVector;
+		FVector saved_horizontalMagnitude;
+
+		int8 safe_flapInitThrust;
+
+		
 	};
+
+	// Each force is an acceleration that we apply to the velocity of their corresponding axes
+	// They don't accumulate, the velocity accumulates them
+	// They're calculated each frame they're needed off of the flight parameters and other data
+	// They're floats because the magnitude of gravity (GravityZ) is
+	float safe_thrustScale;
+	float safe_liftScale;
+	float safe_dragScale;
+
+	int8 safe_angleOfAttack;
+	int8 safe_surfaceArea;
+	float safe_airflowVelocity;
+	float safe_airDensity;
+
+	bool safe_bFlapInput;
+
+	FVector safe_actorForward;
+	float safe_horizontalVelocity;
+
+	int8 safe_flapInitThrust;
 
 	// Set our own owner class so we can access the extra stuff we added to our Character child class (BalingaBase)
 	UPROPERTY() TObjectPtr<ABalingaBase> BalingaOwner; 
 
-	
-
+	void PhysFly(float deltaTIme, int32 Iterations);
 
 
 protected:
