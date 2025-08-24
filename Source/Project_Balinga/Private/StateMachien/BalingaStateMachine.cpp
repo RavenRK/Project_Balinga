1aa521bb4979be03609a7cbb58d5ae5cd7481e98
RavenRK <jamesjiang.workemail@gmail.com>
2025-08-24 13:23:08 +0100

    New file Lay out
    > added files for item state machine and balinga

---
+++ Source/Project_Balinga/Private/StateMachien/BalingaStateMachine.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BalingaStateMachine.h"
+
+// Sets default values
+ABalingaStateMachine::ABalingaStateMachine()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABalingaStateMachine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABalingaStateMachine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Project_Balinga/Public/StateMachine/StateBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "StateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_BALINGA_API UStateBase : public UObject
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	bool bCanTickStae = false;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	bool bCanRepeat = false;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	FName StateName;
+
+	virtual void OnEnterState(AActor* StateOwner);
+	virtual void OnTickState();
+	virtual void OnExitState();
+};
